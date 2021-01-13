789e75c799d6fe6580b1fb300dc6da2bb41f5119
anhquanvgu <14338@student.vgu.edu.vn>
2021-01-13 10:53:12 +0700

    First commit to fork: - Use utm library instead of Havesine formula - Fixed elements' parameters that are not up to Gazebo requirements - Add GetSDF.setOffset() function to offset the the entire model

--- source/dict2sdf.py
+++ source/dict2sdf.py
@@ -11,6 +11,7 @@
 import lxml.etree as Et
 import xml.dom.minidom as minidom
 import numpy
+import utm
 
 
 class GetSDF:
@@ -21,9 +22,67 @@ class GetSDF:
         world = Et.SubElement(self.sdf, 'world')
         world.set('name', 'default')
         self.modelList = dict()
+        self.offset = list()
+    
+    def setOffset(self, boundingbox):
+        utm_coord = list()
+        map_coord = [float(x) for x in boundingbox]
+        utm_coord.extend(utm.from_latlon(map_coord[3], map_coord[2])[:2])
+        utm_coord.extend(utm.from_latlon(map_coord[1], map_coord[0])[:2])
 
-    def addSphericalCoords(self, latVal, lonVal,
-                           elevationVal=0.0, headingVal=0):
+        #self.offset.extend([numpy.abs(utm_coord[0] - utm_coord[2])/2,\
+        #                   numpy.abs(utm_coord[1] - utm_coord[3])/2])
+        self.offset.extend([0,0])
+        print(self.offset)
+    def addSky(self):
+        data_sky = """<scene>
+      <sky>
+        <clouds>
+          <speed>12</speed>
+        </clouds>
+      </sky>
+    </scene>"""
+        self.sdf.xpath('//world')[0].append(Et.fromstring(data_sky))
+    def addGroundPlane(self, boundingbox):
+        data = """    <model name="ground">
+      <link name="body">
+          <collision name="geom">
+              <geometry>
+                  <plane>
+                      <normal>0 0 1</normal>
+                      <size></size>
+                  </plane>
+              </geometry>
+              <surface>
+                  <friction>
+                    <ode>
+                      <mu>100</mu>
+                      <mu2>50</mu2>
+                    </ode>
+                  </friction>
+              </surface>
+          </collision>
+      </link>
+      <static>true</static>
+    </model>
+        """
+        xml_data = Et.fromstring(data)
+
+        size = xml_data.xpath('//size')
+        size[0].text = str(self.offset[0]*2) + " " + str(self.offset[1]*2)
+        self.sdf.xpath('//world')[0].append(xml_data)
+        
+        
+
+    def addSphericalCoords(self, boundingbox):
+        utm_coord = list()
+        map_coord = [float(x) for x in boundingbox]
+        utm_coord.extend(utm.from_latlon(map_coord[1], map_coord[0])[:2])
+        utm_coord.extend(utm.from_latlon(map_coord[3], map_coord[2]))
+        latVal, lonVal = utm.to_latlon(utm_coord[2] + numpy.abs(utm_coord[0] - utm_coord[2])/2,\
+                       utm_coord[3] + numpy.abs(utm_coord[1] - utm_coord[3])/2,\
+                       utm_coord[4], utm_coord[5])
+
         ''' Add the spherical coordinates for the map'''
         spherical_coordinates = Et.SubElement(self.sdf.find('world'),
                                               'spherical_coordinates')
@@ -38,10 +97,10 @@ class GetSDF:
         lon.text = str(lonVal)
 
         elevation = Et.SubElement(spherical_coordinates, 'elevation')
-        elevation.text = str(elevationVal)
+        elevation.text = str(0)
 
         heading = Et.SubElement(spherical_coordinates, 'heading_deg')
-        heading.text = str(headingVal)
+        heading.text = str(0)
 
     def includeModel(self, modelName):
         ''' Include models in gazebo database'''
@@ -63,8 +122,8 @@ class GetSDF:
 
         modelPose = Et.SubElement(includeModel, 'pose')
 
-        modelPose.text = (str(pose[0]) +
-                          " " + str(pose[1]) +
+        modelPose.text = (str(pose[0] - self.offset[0]) +
+                          " " + str(pose[1] - self.offset[1]) +
                           " " + str(pose[2]) + " 0 0 0")
 
     def addRoad(self, roadName):
@@ -80,7 +139,7 @@ class GetSDF:
                       if road.get('name') == roadName]
 
         roadWidth = Et.SubElement(roadWanted[0], 'width')
-        roadWidth.text = str(width)
+        roadWidth.text = str(10)
 
     def addRoadPoint(self, point, roadName):
         '''Add points required to build a road, specified by the roadname'''
@@ -89,8 +148,8 @@ class GetSDF:
         roadWanted = [road for road in allRoads
                       if road.get('name') == roadName]
         roadPoint = Et.SubElement(roadWanted[0], 'point')
-        roadPoint.text = (str(point[0]) +
-                          " " + str(point[1]) +
+        roadPoint.text = (str(point[0] - self.offset[0]) +
+                          " " + str(point[1] - self.offset[1]) +
                           " " + str(point[2]))
 
     def addBuilding(self, mean, pointList, building_name, color):
@@ -99,8 +158,9 @@ class GetSDF:
         static = Et.SubElement(building, 'static')
         static.text = 'true'
         mainPose = Et.SubElement(building, 'pose')
-        mainPose.text = (str(mean[0, 0]) +
-                         " " + str(mean[1, 0]) +
+
+        mainPose.text = (str(mean[0, 0] - self.offset[0]) +
+                         " " + str(mean[1, 0] - self.offset[1]) +
                          " " + str(mean[2, 0]) +
                          " 0 0 0")
 
@@ -129,14 +189,14 @@ class GetSDF:
 
             geometry = Et.SubElement(collision, 'geometry')
             box = Et.SubElement(geometry, 'box')
-            Et.SubElement(box, 'size').text = str(distance[point]) + ' 0.2 0'
+            Et.SubElement(box, 'size').text = str(distance[point]) + ' 0.2 10'
 
             visual = Et.SubElement(link, 'visual')
             visual.set('name', (building_name + '_' + str(point)))
 
             geometry = Et.SubElement(visual, 'geometry')
             box = Et.SubElement(geometry, 'box')
-            Et.SubElement(box, 'size').text = str(distance[point]) + ' 0.2 0'
+            Et.SubElement(box, 'size').text = str(distance[point]) + ' 0.2 10'
 
             material = Et.SubElement(visual, 'material')
             script = Et.SubElement(material, 'script')
